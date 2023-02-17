076fcd07db2d6f0db7a239f9658f22dbe1506dfc
RAMIREZ, DAVID <dr187t@exo.att.com>
2023-02-17 15:55:31 +0800

    feat: display user profile on member only page and public page

--- src/pages/protected-page.js
+++ src/pages/protected-page.js
@@ -6,13 +6,22 @@ import { getProtectedResource } from "../services/message.service";
 
 export const ProtectedPage = () => {
   const [message, setMessage] = useState("");
+  const [userInfo, setUserInfo] = useState("");
 
-  const { getAccessTokenSilently } = useAuth0();
+  const { getAccessTokenSilently, isAuthenticated, user } = useAuth0();
 
   useEffect(() => {
     let isMounted = true;
 
     const getMessage = async () => {
+      if (!isAuthenticated) {
+        setUserInfo(JSON.stringify("Guest", null, 2));
+        setMessage(JSON.stringify("Unauthorized", null, 2));
+        return;
+      } else {
+        setUserInfo(JSON.stringify(user, null, 2));
+      }
+
       const accessToken = await getAccessTokenSilently();
       const { data, error } = await getProtectedResource(accessToken);
 
@@ -49,9 +58,24 @@ export const ProtectedPage = () => {
               external API.
             </span>
             <span>
-              <strong>Only authenticated users can access this page.</strong>
+              <strong>Only authenticated users can access the message from this page.</strong>
             </span>
           </p>
+          { isAuthenticated &&
+          <div className="profile-grid">
+            <div className="profile__header">
+              <img
+                src={user.picture}
+                alt="Profile"
+                className="profile__avatar"
+              />
+              <div className="profile__headline">
+                <h2 className="profile__title">{user.name}</h2>
+                <span className="profile__description">{user.email}</span>
+              </div>
+            </div>
+          </div>}
+          <CodeSnippet title="User Info" code={userInfo} />
           <CodeSnippet title="Protected Message" code={message} />
         </div>
       </div>
--- src/pages/public-page.js
+++ src/pages/public-page.js
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React, { useEffect, useState } from "react";
 import { CodeSnippet } from "../components/code-snippet";
 import { PageLayout } from "../components/page-layout";
@@ -5,11 +6,22 @@ import { getPublicResource } from "../services/message.service";
 
 export const PublicPage = () => {
   const [message, setMessage] = useState("");
+  const [userInfo, setUserInfo] = useState("");
+
+  const { isAuthenticated, user } = useAuth0();
+
 
   useEffect(() => {
     let isMounted = true;
 
     const getMessage = async () => {
+      if (!isAuthenticated) {
+        setUserInfo(JSON.stringify("Guest", null, 2));
+        return;
+      } else {
+        setUserInfo(JSON.stringify(user, null, 2));
+      }
+
       const { data, error } = await getPublicResource();
 
       if (!isMounted) {
@@ -48,6 +60,21 @@ export const PublicPage = () => {
               <strong>Any visitor can access this page.</strong>
             </span>
           </p>
+          { isAuthenticated &&
+          <div className="profile-grid">
+            <div className="profile__header">
+              <img
+                src={user.picture}
+                alt="Profile"
+                className="profile__avatar"
+              />
+              <div className="profile__headline">
+                <h2 className="profile__title">{user.name}</h2>
+                <span className="profile__description">{user.email}</span>
+              </div>
+            </div>
+          </div>}
+          <CodeSnippet title="User Info" code={userInfo} />
           <CodeSnippet title="Public Message" code={message} />
         </div>
       </div>
